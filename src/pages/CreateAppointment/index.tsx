4432cf88a96cef43750bc0401e71587994534f37
Fabio Niglio <fabioniglio@gmail.com>
2020-06-22 00:21:23 +0200

    Create appointment page

--- src/pages/CreateAppointment/index.tsx
+++ src/pages/CreateAppointment/index.tsx
@@ -1,9 +1,60 @@
-import React from 'react';
+import React, { useCallback, useEffect } from 'react';
+import { useRoute, useNavigation } from '@react-navigation/native';
+import Icon from 'react-native-vector-icons/Feather';
+import api from '../../services/api';
 
-import { View } from 'react-native';
+import {
+  Container,
+  Header,
+  BackButton,
+  HeaderTitle,
+  UseAvatar,
+} from './styles';
+import { useAuth } from '../../hooks/auth';
+
+interface RouteParams {
+  providerId: string;
+}
+
+export interface Provider {
+  id: string;
+  name: string;
+  avatar_url: string;
+}
 
 const CreateAppointment: React.FC = () => {
-  return <View />;
+  const { user } = useAuth();
+  const route = useRoute();
+
+  const { goBack } = useNavigation();
+
+  const { providerId } = route.params as RouteParams;
+
+  const [providers, setProviders] = useState<Provider[]>([]);
+
+  useEffect(() => {
+    api.get('providers').then((response) => {
+      setProviders(response.data);
+    });
+  }, []);
+
+  const navigateBack = useCallback(() => {
+    goBack();
+  }, [goBack]);
+
+  return (
+    <Container>
+      <Header>
+        <BackButton onPress={navigateBack}>
+          <Icon name="chevron-left" size={24} color="#999591" />
+        </BackButton>
+
+        <HeaderTitle>Barbers</HeaderTitle>
+
+        <UseAvatar source={{ uri: user.avatar_url }} />
+      </Header>
+    </Container>
+  );
 };
 
 export default CreateAppointment;
